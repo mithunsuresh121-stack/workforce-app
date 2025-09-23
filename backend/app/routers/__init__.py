6c65b2baebe194c09d95855648d67061c3719f10
Mithun Suresh <mithunsuresh@domain.com>
2025-09-23 13:15:50 +0400

    empl usr profile page

--- backend/app/routers/__init__.py
+++ backend/app/routers/__init__.py
@@ -2,7 +2,7 @@ from .auth import router as auth_router
 from .tasks import router as tasks_router
 from .companies import router as companies_router
 from .dashboard import router as dashboard_router
-from .profile import router as profile_router
+from .profile_final import router as profile_router
 from .employees import router as employees_router
 from .leaves import router as leaves_router
 from .shifts import router as shifts_router
